423be5007bd3f35888ff91602108df8c3be3ef20
Mattias Tofte <mattiastofte@gmail.com>
2020-11-13 16:32:32 +0100

    implementere regreson av sinusoid

--- databehandling/integrasjon.py
+++
@@ -1,9 +0,0 @@
-from pylab import *
-data = loadtxt('solflekker.txt', skiprows=1)
-
-tid = data[:,0]
-solflekker = data[:,1]
-
-plot(tid,solflekker,color='forestgreen')
-xlabel('Tid (mnd)')
-ylabel('Gjennomsnittlig av solflekker')
